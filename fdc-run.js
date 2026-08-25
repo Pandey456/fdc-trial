8547bb8b02f4a4b524865a041f7f7294ee1c78b1
Adarsh Pandey <pandey.adarsh2910@gmail.com>
2026-08-25 20:33:21 +0530

    updted req

--- fdc-run.js
+++ fdc-run.js
@@ -115,7 +115,7 @@ async function prepareRequest({ token, startTime, deadline, interval }) {
     // min(low)
 
     postProcessJq:
-      "{ maxPrice: ((sort_by(.[2] | tonumber) | reverse | .[0][2] | tonumber) * 100000000 | floor), minPrice: ((sort_by(.[3] | tonumber) | .[0][3] | tonumber) * 100000000 | floor) }",
+      "{ maxPrice: (([.[][2] | tonumber] | max) * 100000000 | floor), minPrice: (([.[][3] | tonumber] | min) * 100000000 | floor) }",
 
     abiSignature: JSON.stringify({
       type: "tuple",
